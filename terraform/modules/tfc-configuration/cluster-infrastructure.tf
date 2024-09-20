ec0002ce20ad5b83559437a0890359708dad002f
neilha <neilharrold@gmail.com>
2024-09-20 15:21:24 +0100

    adds cluster-infrastructure workspace

---
+++ terraform/modules/tfc-configuration/cluster-infrastructure.tf
@@ -0,0 +1,28 @@
+module "cluster-infrastructure-production" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.10.0"
+
+  organization        = var.organization
+  workspace_name      = "cluster-infrastructure-production"
+  workspace_desc      = "This module manages the EKS cluster, and other resources it depends on (e.g. IAM roles and policies)"
+  workspace_tags      = ["production", "cluster-infrastructure", "eks", "aws"]
+  terraform_version   = var.terraform_version
+  execution_mode      = "remote"
+  working_directory   = "/terraform/modules/cluster-infrastructure/"
+  trigger_patterns    = ["/terraform/modules/cluster-infrastructure/**/*"]
+  global_remote_state = true
+
+  project_name = "publishing-platform-infrastructure"
+
+  vcs_repo = {
+    identifier     = "publishing-platform/publishing-platform-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  variable_set_names = [
+    # "aws-credentials-production",
+    # "common",
+    "common-production"
+  ]
+}
