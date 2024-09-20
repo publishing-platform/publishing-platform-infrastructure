57027151b351e2ab0f0cba22757a3dadb7ff3f00
neilha <neilharrold@gmail.com>
2024-09-20 15:17:32 +0100

    adds projects

--- terraform/modules/tfc-configuration/variables.tf
+++ terraform/modules/tfc-configuration/variables.tf
@@ -7,3 +7,23 @@ variable "organization" {
   description = "Name of TFC Organization that the workspace will belong to"
   default     = "publishing-platform"
 }
+
+#
+# Projects
+#
+
+variable "project_names" {
+  description = "List of project names"
+  type        = list(string)
+  default     = ["publishing-platform-infrastructure"]
+}
+
+#
+# Workspace
+#
+
+variable "terraform_version" {
+  type        = string
+  description = "Version constraint for Terraform for this workspace."
+  default     = "~> 1.9.0"
+}
