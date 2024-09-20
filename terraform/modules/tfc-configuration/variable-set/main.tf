ca2533c031f5c121944f552a3bf64910912bb6c0
neilha <neilharrold@gmail.com>
2024-09-20 11:14:05 +0100

    typo

--- terraform/modules/tfc-configuration/variable-set/main.tf
+++ terraform/modules/tfc-configuration/variable-set/main.tf
@@ -1,6 +1,6 @@
 resource "tfe_variable_set" "set" {
   name         = var.name
-  organization = var.organizaton
+  organization = var.organization
   priority     = var.priority
   global       = false
 }
