f4977a76d1908b886117943716cffd9d792f8963
neilha <neilharrold@gmail.com>
2024-09-25 19:33:33 +0100

    deletes vpc workspace

--- terraform/modules/vpc/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vpc_id" {
-  value       = aws_vpc.vpc.id
-  description = "The ID of the VPC."
-}
-
-output "internet_gateway_id" {
-  value       = aws_internet_gateway.public.id
-  description = "The ID of the Internet Gateway."
-}
--- terraform/modules/vpc/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_cidr" {
-  type        = string
-  description = "The CIDR block for the VPC."
-}
-
-variable "publishing_platform_environment" {
-  type        = string
-  description = "Publishing Platform environment where resources are being deployed"
-}
