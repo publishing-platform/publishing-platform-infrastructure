9c44f9c3fb86a4d65f5bcdb31be66c001e7a0ac1
neilha <neilharrold@gmail.com>
2024-10-25 20:15:49 +0100

    debugging

--- terraform/modules/cluster-infrastructure/main.tf
+++ terraform/modules/cluster-infrastructure/main.tf
@@ -26,71 +26,71 @@ locals {
   }
 }
 
-data "aws_iam_policy_document" "node_assumerole" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
+# data "aws_iam_policy_document" "node_assumerole" {
+#   statement {
+#     actions = ["sts:AssumeRole"]
+#     principals {
+#       type        = "Service"
+#       identifiers = ["ec2.amazonaws.com"]
+#     }
+#   }
+# }
 
-resource "aws_iam_role" "node" {
-  description           = "EKS managed node group IAM role"
-  assume_role_policy    = data.aws_iam_policy_document.node_assumerole.json
-  force_detach_policies = true
-}
+# resource "aws_iam_role" "node" {
+#   description           = "EKS managed node group IAM role"
+#   assume_role_policy    = data.aws_iam_policy_document.node_assumerole.json
+#   force_detach_policies = true
+# }
 
-data "aws_iam_policy_document" "pull_from_ecr" {
-  statement {
-    actions = [
-      "ecr:GetAuthorizationToken",
-      "ecr:BatchCheckLayerAvailability",
-      "ecr:GetDownloadUrlForLayer",
-      "ecr:GetRepositoryPolicy",
-      "ecr:DescribeRepositories",
-      "ecr:ListImages",
-      "ecr:DescribeImages",
-      "ecr:BatchGetImage",
-      "ecr:BatchImportUpstreamImage",
-      "ecr:GetLifecyclePolicy",
-      "ecr:GetLifecyclePolicyPreview",
-      "ecr:ListTagsForResource",
-      "ecr:DescribeImageScanFindings"
-    ]
+# data "aws_iam_policy_document" "pull_from_ecr" {
+#   statement {
+#     actions = [
+#       "ecr:GetAuthorizationToken",
+#       "ecr:BatchCheckLayerAvailability",
+#       "ecr:GetDownloadUrlForLayer",
+#       "ecr:GetRepositoryPolicy",
+#       "ecr:DescribeRepositories",
+#       "ecr:ListImages",
+#       "ecr:DescribeImages",
+#       "ecr:BatchGetImage",
+#       "ecr:BatchImportUpstreamImage",
+#       "ecr:GetLifecyclePolicy",
+#       "ecr:GetLifecyclePolicyPreview",
+#       "ecr:ListTagsForResource",
+#       "ecr:DescribeImageScanFindings"
+#     ]
 
-    effect    = "Allow"
-    resources = ["*"]
-  }
-}
+#     effect    = "Allow"
+#     resources = ["*"]
+#   }
+# }
 
-resource "aws_iam_policy" "pull_from_ecr" {
-  name        = "pull-from-ecr"
-  description = "Policy to allows EKS to pull images from ECR"
-  policy      = data.aws_iam_policy_document.pull_from_ecr.json
-}
+# resource "aws_iam_policy" "pull_from_ecr" {
+#   name        = "pull-from-ecr"
+#   description = "Policy to allows EKS to pull images from ECR"
+#   policy      = data.aws_iam_policy_document.pull_from_ecr.json
+# }
 
-resource "aws_iam_role_policy_attachment" "pull_from_ecr" {
-  policy_arn = aws_iam_policy.pull_from_ecr.arn
-  role       = aws_iam_role.node.name
-}
+# resource "aws_iam_role_policy_attachment" "pull_from_ecr" {
+#   policy_arn = aws_iam_policy.pull_from_ecr.arn
+#   role       = aws_iam_role.node.name
+# }
 
-resource "aws_iam_role_policy_attachment" "node" {
-  for_each = toset([
-    "AmazonEKSWorkerNodePolicy",
-    "AmazonEKS_CNI_Policy",
-    "AmazonSSMManagedInstanceCore",
-  ])
-  policy_arn = "arn:aws:iam::aws:policy/${each.key}"
-  role       = aws_iam_role.node.name
-}
+# resource "aws_iam_role_policy_attachment" "node" {
+#   for_each = toset([
+#     "AmazonEKSWorkerNodePolicy",
+#     "AmazonEKS_CNI_Policy",
+#     "AmazonSSMManagedInstanceCore",
+#   ])
+#   policy_arn = "arn:aws:iam::aws:policy/${each.key}"
+#   role       = aws_iam_role.node.name
+# }
 
-resource "aws_kms_key" "eks" {
-  description             = "EKS Secret Encryption Key"
-  deletion_window_in_days = 7
-  enable_key_rotation     = true
-}
+# resource "aws_kms_key" "eks" {
+#   description             = "EKS Secret Encryption Key"
+#   deletion_window_in_days = 7
+#   enable_key_rotation     = true
+# }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
@@ -113,12 +113,12 @@ module "eks" {
     "api", "audit", "authenticator", "controllerManager", "scheduler"
   ]
 
-  cluster_encryption_config = {
-    provider_key_arn = aws_kms_key.eks.arn
-    resources        = ["secrets"]
-  }
-  create_kms_key                = false
-  kms_key_enable_default_policy = false
+  # cluster_encryption_config = {
+  #   provider_key_arn = aws_kms_key.eks.arn
+  #   resources        = ["secrets"]
+  # }
+  # create_kms_key                = false
+  # kms_key_enable_default_policy = false
 
   # We're just using the cluster primary SG as created by EKS.
   create_cluster_security_group = false
@@ -131,8 +131,8 @@ module "eks" {
     capacity_type         = var.workers_default_capacity_type
     subnet_ids            = [for s in aws_subnet.eks_private : s.id]
     create_security_group = false
-    create_iam_role       = false
-    iam_role_arn          = aws_iam_role.node.arn
+    # create_iam_role       = false
+    # iam_role_arn          = aws_iam_role.node.arn
   }
 
   eks_managed_node_groups = local.main_managed_node_group
