1e25e6cb37bdce52b2766811470eb954090c577d
neilha <neilharrold@gmail.com>
2024-10-25 21:24:52 +0100

    reverts changes

--- terraform/modules/cluster-infrastructure/main.tf
+++ terraform/modules/cluster-infrastructure/main.tf
@@ -1,173 +1,139 @@
-# locals {
-#   main_managed_node_group = {
-#     main = {
-#       name_prefix    = var.cluster_name
-#       desired_size   = var.workers_size_desired
-#       max_size       = var.workers_size_max
-#       min_size       = var.workers_size_min
-#       instance_types = var.workers_instance_types
-#       update_config  = { max_unavailable = 1 }
-#       block_device_mappings = {
-#         xvda = {
-#           device_name = "/dev/xvda"
-#           ebs = {
-#             volume_size           = var.node_disk_size
-#             volume_type           = "gp3"
-#             encrypted             = true
-#             delete_on_termination = true
-#           }
-#         }
-#       }
-#       additional_tags = {
-#         "k8s.io/cluster-autoscaler/enabled"             = "true"
-#         "k8s.io/cluster-autoscaler/${var.cluster_name}" = "owned"
-#       }
-#     }
-#   }
-# }
+locals {
+  main_managed_node_group = {
+    main = {
+      name_prefix    = var.cluster_name
+      desired_size   = var.workers_size_desired
+      max_size       = var.workers_size_max
+      min_size       = var.workers_size_min
+      instance_types = var.workers_instance_types
+      update_config  = { max_unavailable = 1 }
+      block_device_mappings = {
+        xvda = {
+          device_name = "/dev/xvda"
+          ebs = {
+            volume_size           = var.node_disk_size
+            volume_type           = "gp3"
+            encrypted             = true
+            delete_on_termination = true
+          }
+        }
+      }
+      additional_tags = {
+        "k8s.io/cluster-autoscaler/enabled"             = "true"
+        "k8s.io/cluster-autoscaler/${var.cluster_name}" = "owned"
+      }
+    }
+  }
+}
 
-# data "aws_iam_policy_document" "node_assumerole" {
-#   statement {
-#     actions = ["sts:AssumeRole"]
-#     principals {
-#       type        = "Service"
-#       identifiers = ["ec2.amazonaws.com"]
-#     }
-#   }
-# }
+data "aws_iam_policy_document" "node_assumerole" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
 
-# resource "aws_iam_role" "node" {
-#   description           = "EKS managed node group IAM role"
-#   assume_role_policy    = data.aws_iam_policy_document.node_assumerole.json
-#   force_detach_policies = true
-# }
+resource "aws_iam_role" "node" {
+  description           = "EKS managed node group IAM role"
+  assume_role_policy    = data.aws_iam_policy_document.node_assumerole.json
+  force_detach_policies = true
+}
 
-# data "aws_iam_policy_document" "pull_from_ecr" {
-#   statement {
-#     actions = [
-#       "ecr:GetAuthorizationToken",
-#       "ecr:BatchCheckLayerAvailability",
-#       "ecr:GetDownloadUrlForLayer",
-#       "ecr:GetRepositoryPolicy",
-#       "ecr:DescribeRepositories",
-#       "ecr:ListImages",
-#       "ecr:DescribeImages",
-#       "ecr:BatchGetImage",
-#       "ecr:BatchImportUpstreamImage",
-#       "ecr:GetLifecyclePolicy",
-#       "ecr:GetLifecyclePolicyPreview",
-#       "ecr:ListTagsForResource",
-#       "ecr:DescribeImageScanFindings"
-#     ]
+data "aws_iam_policy_document" "pull_from_ecr" {
+  statement {
+    actions = [
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:BatchImportUpstreamImage",
+      "ecr:GetLifecyclePolicy",
+      "ecr:GetLifecyclePolicyPreview",
+      "ecr:ListTagsForResource",
+      "ecr:DescribeImageScanFindings"
+    ]
 
-#     effect    = "Allow"
-#     resources = ["*"]
-#   }
-# }
+    effect    = "Allow"
+    resources = ["*"]
+  }
+}
 
-# resource "aws_iam_policy" "pull_from_ecr" {
-#   name        = "pull-from-ecr"
-#   description = "Policy to allows EKS to pull images from ECR"
-#   policy      = data.aws_iam_policy_document.pull_from_ecr.json
-# }
+resource "aws_iam_policy" "pull_from_ecr" {
+  name        = "pull-from-ecr"
+  description = "Policy to allows EKS to pull images from ECR"
+  policy      = data.aws_iam_policy_document.pull_from_ecr.json
+}
 
-# resource "aws_iam_role_policy_attachment" "pull_from_ecr" {
-#   policy_arn = aws_iam_policy.pull_from_ecr.arn
-#   role       = aws_iam_role.node.name
-# }
+resource "aws_iam_role_policy_attachment" "pull_from_ecr" {
+  policy_arn = aws_iam_policy.pull_from_ecr.arn
+  role       = aws_iam_role.node.name
+}
 
-# resource "aws_iam_role_policy_attachment" "node" {
-#   for_each = toset([
-#     "AmazonEKSWorkerNodePolicy",
-#     "AmazonEKS_CNI_Policy",
-#     "AmazonSSMManagedInstanceCore",
-#   ])
-#   policy_arn = "arn:aws:iam::aws:policy/${each.key}"
-#   role       = aws_iam_role.node.name
-# }
+resource "aws_iam_role_policy_attachment" "node" {
+  for_each = toset([
+    "AmazonEKSWorkerNodePolicy",
+    "AmazonEKS_CNI_Policy",
+    "AmazonSSMManagedInstanceCore",
+  ])
+  policy_arn = "arn:aws:iam::aws:policy/${each.key}"
+  role       = aws_iam_role.node.name
+}
 
-# resource "aws_kms_key" "eks" {
-#   description             = "EKS Secret Encryption Key"
-#   deletion_window_in_days = 7
-#   enable_key_rotation     = true
-# }
+resource "aws_kms_key" "eks" {
+  description             = "EKS Secret Encryption Key"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+}
 
-# module "eks" {
-#   source  = "terraform-aws-modules/eks/aws"
-#   version = "~> 20.0"
-
-#   cluster_name    = var.cluster_name
-#   cluster_version = var.cluster_version
-#   subnet_ids      = [for s in aws_subnet.eks_control_plane : s.id]
-#   vpc_id          = aws_vpc.vpc.id
-
-#   cluster_addons = {
-#     coredns    = { most_recent = true }
-#     kube-proxy = { most_recent = true }
-#     vpc-cni    = { most_recent = true }
-#   }
-
-#   cluster_endpoint_public_access         = true
-#   cloudwatch_log_group_retention_in_days = var.cluster_log_retention_in_days
-#   cluster_enabled_log_types = [
-#     "api", "audit", "authenticator", "controllerManager", "scheduler"
-#   ]
-
-#   cluster_encryption_config = {
-#     provider_key_arn = aws_kms_key.eks.arn
-#     resources        = ["secrets"]
-#   }
-#   create_kms_key                = false
-#   kms_key_enable_default_policy = false
-
-#   # We're just using the cluster primary SG as created by EKS.
-#   create_cluster_security_group = false
-#   create_node_security_group    = false
-
-#   authentication_mode = "API_AND_CONFIG_MAP" # see https://github.com/terraform-aws-modules/terraform-aws-eks/issues/3026
-
-#   eks_managed_node_group_defaults = {
-#     ami_type              = "AL2023_x86_64_STANDARD"
-#     capacity_type         = var.workers_default_capacity_type
-#     subnet_ids            = [for s in aws_subnet.eks_private : s.id]
-#     create_security_group = false
-#     create_iam_role       = false
-#     iam_role_arn          = aws_iam_role.node.arn
-#   }
-
-#   eks_managed_node_groups = local.main_managed_node_group
-# }
-
-module "eks_al2023" {
+module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "testing-al2023"
-  cluster_version = "1.31"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  subnet_ids      = [for s in aws_subnet.eks_control_plane : s.id]
+  vpc_id          = aws_vpc.vpc.id
 
-  # EKS Addons
   cluster_addons = {
-    coredns                = {}
-    eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
+    coredns    = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni    = { most_recent = true }
   }
 
-  subnet_ids = [for s in aws_subnet.eks_control_plane : s.id]
-  vpc_id     = aws_vpc.vpc.id
+  cluster_endpoint_public_access         = true
+  cloudwatch_log_group_retention_in_days = var.cluster_log_retention_in_days
+  cluster_enabled_log_types = [
+    "api", "audit", "authenticator", "controllerManager", "scheduler"
+  ]
 
-  eks_managed_node_groups = {
-    example = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      instance_types = ["t3.small"]
+  cluster_encryption_config = {
+    provider_key_arn = aws_kms_key.eks.arn
+    resources        = ["secrets"]
+  }
+  create_kms_key                = false
+  kms_key_enable_default_policy = false
 
-      min_size = 2
-      max_size = 5
-      # This value is ignored after the initial creation
-      # https://github.com/bryantbiggs/eks-desired-size-hack
-      desired_size = 2
-    }
+  # We're just using the cluster primary SG as created by EKS.
+  create_cluster_security_group = false
+  create_node_security_group    = false
+
+  authentication_mode = "API_AND_CONFIG_MAP" # see https://github.com/terraform-aws-modules/terraform-aws-eks/issues/3026
+
+  eks_managed_node_group_defaults = {
+    ami_type              = "AL2023_x86_64_STANDARD"
+    capacity_type         = var.workers_default_capacity_type
+    subnet_ids            = [for s in aws_subnet.eks_private : s.id]
+    create_security_group = false
+    create_iam_role       = false
+    iam_role_arn          = aws_iam_role.node.arn
   }
 
-  enable_cluster_creator_admin_permissions = true
+  eks_managed_node_groups = local.main_managed_node_group
 }
