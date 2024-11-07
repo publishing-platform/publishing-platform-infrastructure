836ff64e073f1442e8503d8b06e63079b432f9d9
neilha <neilharrold@gmail.com>
2024-11-07 20:41:39 +0000

    adds ecr workspace and variable set

---
+++ terraform/modules/tfc-configuration/ecr.tf
@@ -0,0 +1,30 @@
+// There will only ever be a production ecr workspace.  All container images are stored in production AWS account.
+module "ecr-production" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.10.0"
+
+  organization        = var.organization
+  workspace_name      = "ecr-production"
+  workspace_desc      = "This module manages Elastic Container Registry repositories, to store OCI images of Publishing Platform apps"
+  workspace_tags      = ["production", "ecr", "eks", "aws"]
+  terraform_version   = var.terraform_version
+  execution_mode      = "remote"
+  working_directory   = "/terraform/modules/ecr/"
+  trigger_patterns    = ["/terraform/modules/ecr/**/*"]
+  global_remote_state = true
+
+  project_name = "publishing-platform-infrastructure"
+
+  vcs_repo = {
+    identifier     = "publishing-platform/publishing-platform-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  variable_set_names = [
+    "aws-credentials-production",
+    # "common",
+    "common-production",
+    "ecr-production"
+  ]
+}
