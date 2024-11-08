ed3e4c2c01ae6f8ac8e4ed8168aabdf167e5b167
neilha <neilharrold@gmail.com>
2024-11-08 17:34:44 +0000

    delegates creation of tfc-aws-config workspaces to tfc-configuration module

---
+++ terraform/modules/tfc-configuration/tfc-aws-config.tf
@@ -0,0 +1,14 @@
+module "tfc-aws-config-production" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.10.0"
+
+  organization        = var.organization
+  workspace_name      = "ecr-production"
+  workspace_desc      = "Sets up the OpenID Connect authentication and AWS IAM authorisation for the publishing-platform Terraform Cloud org to manage resources in the Publishing Platform AWS accounts."
+  workspace_tags      = ["production", "tfc", "aws", "configuration"]
+  terraform_version   = var.terraform_version
+  execution_mode      = "local"
+  global_remote_state = true
+
+  project_name = "publishing-platform-infrastructure"
+}
