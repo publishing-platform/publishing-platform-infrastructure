05089b2406eff1b1f92e259e29f03e42f17bcdc8
neilha <neilharrold@gmail.com>
2024-11-08 17:54:34 +0000

    typo

--- terraform/modules/tfc-configuration/tfc-aws-config.tf
+++ terraform/modules/tfc-configuration/tfc-aws-config.tf
@@ -3,7 +3,7 @@ module "tfc-aws-config-production" {
   version = "0.10.0"
 
   organization        = var.organization
-  workspace_name      = "ecr-production"
+  workspace_name      = "tfc-aws-config-production"
   workspace_desc      = "Sets up the OpenID Connect authentication and AWS IAM authorisation for the publishing-platform Terraform Cloud org to manage resources in the Publishing Platform AWS accounts."
   workspace_tags      = ["production", "tfc", "aws", "configuration"]
   terraform_version   = var.terraform_version
