9e6beb1ceacaf29cd7111cf89bc5ab3beff3a83f
neilha <neilharrold@gmail.com>
2024-11-08 12:41:35 +0000

    debugging github_repositories datasource

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,3 +1,7 @@
 data "github_repositories" "publishing_platform" {
   query = "org:publishing-platform topic:container topic:publishing-platform fork:false archived:false"
 }
+
+output "variable_set_id" {
+  value = data.github_repositories.publishing_platform.names
+}
