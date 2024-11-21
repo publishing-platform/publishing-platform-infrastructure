b49a3f1fe99ed48632e9dce69912c32d409edeb5
neilha <neilharrold@gmail.com>
2024-11-21 19:03:24 +0000

    adds rds security group

--- terraform/modules/rds/remote.tf
+++ terraform/modules/rds/remote.tf
@@ -2,3 +2,8 @@ data "tfe_outputs" "vpc" {
   organization = "publishing-platform"
   workspace    = "vpc-${var.publishing_platform_environment}"
 }
+
+data "tfe_outputs" "cluster_infrastructure" {
+  organization = "publishing-platform"
+  workspace    = "cluster-infrastructure-${var.publishing_platform_environment}"
+}
---
+++ terraform/modules/rds/security_groups.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "rds" {
+  for_each = var.databases
+
+  name        = "${each.value.name}_rds_access"
+  vpc_id      = data.tfe_outputs.vpc.nonsensitive_values.vpc_id
+  description = "Access to ${each.value.name} RDS"
+  tags        = { Name = "${each.value.name}_rds_access" }
+}
+
+resource "aws_security_group_rule" "postgres" {
+  for_each          = { for name, data in var.databases : name => data if data.engine == "postgres" }
+  security_group_id = aws_security_group.rds[each.key].id
+  description       = "Access to PostgreSQL database from EKS worker nodes"
+
+  type      = "ingress"
+  protocol  = "tcp"
+  from_port = 5432
+  to_port   = 5432
+
+  source_security_group_id = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.node_security_group_id
+}
