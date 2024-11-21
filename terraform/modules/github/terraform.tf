8d29c2eff58af4119c89947aa4874e7c4c0ebf32
neilha <neilharrold@gmail.com>
2024-11-21 20:52:07 +0000

    removes aws provider from github module as it is no longer used

--- terraform/modules/github/terraform.tf
+++ terraform/modules/github/terraform.tf
@@ -9,10 +9,6 @@ terraform {
   required_version = "~> 1.5"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.5"
-    }
     github = {
       source  = "integrations/github"
       version = "~> 6.0"
