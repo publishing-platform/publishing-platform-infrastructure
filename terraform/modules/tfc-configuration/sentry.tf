b65eef93009afe79dfebe359cb079a1d07d2dca2
neilha <neilharrold@gmail.com>
2024-11-22 12:11:07 +0000

    adds sentry workspace

---
+++ terraform/modules/tfc-configuration/sentry.tf
@@ -0,0 +1,32 @@
+// There will only ever be a production sentry workspace.
+module "sentry-production" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.10.0"
+
+  organization        = var.organization
+  workspace_name      = "sentry-production"
+  workspace_desc      = "This module configures Sentry resources"
+  terraform_version   = var.terraform_version
+  execution_mode      = "remote"
+  working_directory   = "/terraform/modules/sentry/"
+  trigger_patterns    = ["/terraform/modules/sentry/**/*"]
+  global_remote_state = true
+  allow_destroy_plan  = false
+
+  project_name = "publishing-platform-infrastructure"
+
+  vcs_repo = {
+    identifier     = "publishing-platform/publishing-platform-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  variable_set_names = [
+    # "common",
+    "common-production"
+  ]
+
+  depends_on = [
+    module.variable-set-production
+  ]
+}
