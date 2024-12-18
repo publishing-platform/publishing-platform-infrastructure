3074617b17ae7df3283372ef751d3ea6b63f800c
neilha <neilharrold@gmail.com>
2024-12-18 18:00:51 +0000

    continues work on  cluster-services module

--- terraform/modules/cluster-services/providers.tf
+++ terraform/modules/cluster-services/providers.tf
@@ -2,12 +2,6 @@ data "aws_eks_cluster_auth" "cluster_token" {
   name = "publishing-platform"
 }
 
-output "token" {
-  description = "AWS EKS cluster token"
-  value       = data.aws_eks_cluster_auth.cluster_token
-  sensitive = true
-}
-
 provider "aws" {
   region = "eu-west-2"
   default_tags {
@@ -22,18 +16,18 @@ provider "aws" {
   }
 }
 
-# provider "kubernetes" {
-#   host                   = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_endpoint
-#   cluster_ca_certificate = base64decode(data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_certificate_authority_data)
-#   token                  = data.aws_eks_cluster_auth.cluster_token.token
-# }
+provider "kubernetes" {
+  host                   = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_endpoint
+  cluster_ca_certificate = base64decode(data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster_token.token
+}
 
-# provider "helm" {
-#   # TODO: If/when TF makes provider configs a first-class language object,
-#   # reuse the identical config from above.
-#   kubernetes {
-#     host                   = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_endpoint
-#     cluster_ca_certificate = base64decode(data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_certificate_authority_data)
-#     token                  = data.aws_eks_cluster_auth.cluster_token.token
-#   }
-# }
+provider "helm" {
+  # TODO: If/when TF makes provider configs a first-class language object,
+  # reuse the identical config from above.
+  kubernetes {
+    host                   = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_endpoint
+    cluster_ca_certificate = base64decode(data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.cluster_token.token
+  }
+}
