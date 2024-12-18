bd03f4ce084015683d4a63c8c0ad0569a95c8ff5
neilha <neilharrold@gmail.com>
2024-12-18 17:38:37 +0000

    continues work on  cluster-services module

--- terraform/modules/cluster-services/providers.tf
+++ terraform/modules/cluster-services/providers.tf
@@ -2,6 +2,20 @@ data "aws_eks_cluster_auth" "cluster_token" {
   name = "publishing-platform"
 }
 
+provider "aws" {
+  region = "eu-west-2"
+  default_tags {
+    tags = {
+      Product     = "Publishing Platform"
+      System      = "EKS cluster services"
+      Environment = var.publishing_platform_environment
+      Cluster     = var.cluster_name
+      Repository  = "publishing-platform-infrastructure"
+      Workspace   = terraform.workspace
+    }
+  }
+}
+
 # provider "kubernetes" {
 #   host                   = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_endpoint
 #   cluster_ca_certificate = base64decode(data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_certificate_authority_data)
