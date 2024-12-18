c39e92935d11cb54f3a19057f5e6919541197e9d
neilha <neilharrold@gmail.com>
2024-12-18 17:52:51 +0000

    adds cluster_id output variable to cluster-infrastructure module

--- terraform/modules/cluster-infrastructure/outputs.tf
+++ terraform/modules/cluster-infrastructure/outputs.tf
@@ -1,3 +1,8 @@
+output "cluster_id" {
+  description = "The name (also known as the ID) of the EKS cluster."
+  value       = module.eks.cluster_name
+}
+
 output "cluster_certificate_authority_data" {
   description = "Base64-encoded certificate data required to communicate with the cluster."
   value       = module.eks.cluster_certificate_authority_data
