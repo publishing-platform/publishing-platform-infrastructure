e3f9c2d665ab4c593845246086fdb38376b2454c
neilha <neilharrold@gmail.com>
2024-12-18 17:50:42 +0000

    adds cluster_endpoint output variable to cluster-infrastructure module

--- terraform/modules/cluster-infrastructure/outputs.tf
+++ terraform/modules/cluster-infrastructure/outputs.tf
@@ -3,6 +3,11 @@ output "cluster_certificate_authority_data" {
   value       = module.eks.cluster_certificate_authority_data
 }
 
+output "cluster_endpoint" {
+  description = "The endpoint for the EKS cluster's kube-apiserver."
+  value       = module.eks.cluster_endpoint
+}
+
 output "node_security_group_id" {
   description = "ID of the security group which contains the worker nodes. May or may not be the same as control_plane_security_group_id."
   value       = module.eks.cluster_primary_security_group_id
