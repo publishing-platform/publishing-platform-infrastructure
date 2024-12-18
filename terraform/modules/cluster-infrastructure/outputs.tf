0b28abbd52fde609f1c612ca2f141d435d47cb7a
neilha <neilharrold@gmail.com>
2024-12-18 17:28:53 +0000

    adds output variables for aws lb controller

--- terraform/modules/cluster-infrastructure/outputs.tf
+++ terraform/modules/cluster-infrastructure/outputs.tf
@@ -2,3 +2,13 @@ output "node_security_group_id" {
   description = "ID of the security group which contains the worker nodes. May or may not be the same as control_plane_security_group_id."
   value       = module.eks.cluster_primary_security_group_id
 }
+
+output "aws_lb_controller_role_arn" {
+  description = "IAM role ARN corresponding to the k8s service account for the AWS Load Balancer Controller."
+  value       = module.aws_lb_controller_iam_role.iam_role_arn
+}
+
+output "aws_lb_controller_service_account_name" {
+  description = "Name of the k8s service account for the AWS Load Balancer Controller."
+  value       = local.aws_lb_controller_service_account_name
+}
