566ed041548637a0108e5f0f5c6a5532b99b37bf
neilha <neilharrold@gmail.com>
2024-12-19 20:48:17 +0000

    adds helm release for external_secrets

--- terraform/modules/cluster-services/external_secrets.tf
+++ terraform/modules/cluster-services/external_secrets.tf
@@ -3,9 +3,34 @@
 # The AWS IAM resources for this are in
 # ../cluster-infrastructure/external_secrets_iam.tf.
 
+resource "helm_release" "external_secrets" {
+  name             = "external-secrets"
+  repository       = "https://charts.external-secrets.io"
+  chart            = "external-secrets"
+  version          = "0.11.0" # TODO: Dependabot or equivalent so this doesn't get neglected.
+  namespace        = local.services_ns
+  create_namespace = true
+  values = [yamlencode({
+    replicaCount = var.desired_ha_replicas
+    serviceAccount = {
+      name = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.external_secrets_service_account_name
+      annotations = {
+        "eks.amazonaws.com/role-arn" = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.external_secrets_role_arn
+      }
+    }
+    certController = {
+      replicaCount = var.desired_ha_replicas
+    }
+    webhook = {
+      replicaCount = var.desired_ha_replicas
+    }
+  })]
+}
+
 resource "helm_release" "cluster_secret_store" {
   # We require the CRD for ClusterSecretStore from helm_release.external_secrets
   # before this resource can be created.
+  depends_on = [helm_release.external_secrets]
 
   name       = "cluster-secret-store"
   repository = "https://publishing-platform.github.io/publishing-platform-helm-charts/"
