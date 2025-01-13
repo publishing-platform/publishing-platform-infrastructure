b23b28803cd83a64562f530ad6e7df6745e917ea
neilha <neilharrold@gmail.com>
2025-01-13 21:14:02 +0000

    typo

--- terraform/modules/waf/outputs.tf
+++ terraform/modules/waf/outputs.tf
@@ -1,9 +1,9 @@
 output "backend_public_web_acl_arn" {
   description = "ARN corresponding to the backend public Web ACL."
-  value       = aws_wafv2_web_acl.backend_public
+  value       = aws_wafv2_web_acl.backend_public.arn
 }
 
 output "cache_public_web_acl_arn" {
   description = "ARN corresponding to the cache public Web ACL."
-  value       = aws_wafv2_web_acl.cache_public
+  value       = aws_wafv2_web_acl.cache_public.arn
 }
