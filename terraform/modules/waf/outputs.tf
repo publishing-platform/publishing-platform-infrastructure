ca96ac5a55f51feba64ebad1585098539da063a9
neilha <neilharrold@gmail.com>
2025-01-13 21:11:34 +0000

    adds output variables to waf module

---
+++ terraform/modules/waf/outputs.tf
@@ -0,0 +1,9 @@
+output "backend_public_web_acl_arn" {
+  description = "ARN corresponding to the backend public Web ACL."
+  value       = aws_wafv2_web_acl.backend_public
+}
+
+output "cache_public_web_acl_arn" {
+  description = "ARN corresponding to the cache public Web ACL."
+  value       = aws_wafv2_web_acl.cache_public
+}
