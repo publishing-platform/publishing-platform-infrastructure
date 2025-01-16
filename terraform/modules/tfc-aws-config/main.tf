750f6e4a7a14535cbeaacc337b32218628679595
neilha <neilharrold@gmail.com>
2025-01-16 18:09:11 +0000

    allows terraform cloud role iam:PassRole permission

--- terraform/modules/tfc-aws-config/main.tf
+++ terraform/modules/tfc-aws-config/main.tf
@@ -72,6 +72,7 @@ data "aws_iam_policy_document" "tfc_policy" {
       "iam:UntagRole",
       "iam:UpdateRole",
       "iam:SetDefaultPolicyVersion",
+      "iam:PassRole",
       "kms:*",
       "lambda:*",
       "logs:*",
