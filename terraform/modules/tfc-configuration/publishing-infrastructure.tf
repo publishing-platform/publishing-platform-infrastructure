06abce5c85d34f28ed2127c39830c4891cc2395a
neilha <neilharrold@gmail.com>
2025-01-16 14:59:52 +0000

    adds publishing-infrastructure module

---
+++ terraform/modules/tfc-configuration/publishing-infrastructure.tf
@@ -0,0 +1,33 @@
+module "publishing-infrastructure-production" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.10.0"
+
+  organization        = var.organization
+  workspace_name      = "publishing-infrastructure-production"
+  workspace_desc      = "This module manages AWS resources which are specific to Publishing Platform Publishing."
+  workspace_tags      = ["production", "publishing-infrastructure", "eks", "aws"]
+  terraform_version   = var.terraform_version
+  execution_mode      = "remote"
+  working_directory   = "/terraform/modules/publishing-infrastructure/"
+  trigger_patterns    = ["/terraform/modules/publishing-infrastructure/**/*"]
+  global_remote_state = true
+
+  project_name = "publishing-platform-infrastructure"
+
+  vcs_repo = {
+    identifier     = "publishing-platform/publishing-platform-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  variable_set_names = [
+    "aws-credentials-production",
+    # "common",
+    "common-production"
+  ]
+
+  depends_on = [
+    module.variable-set-aws-credentials-production,
+    module.variable-set-production
+  ]
+}
