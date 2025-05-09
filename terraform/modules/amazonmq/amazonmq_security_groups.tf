8eeefbdd605812eb1902a522fd056aa9080e10bc
neilha <neilharrold@gmail.com>
2025-05-09 17:38:24 +0100

    continuers work on  amazonmq

--- terraform/modules/amazonmq/amazonmq_security_groups.tf
+++ terraform/modules/amazonmq/amazonmq_security_groups.tf
@@ -2,4 +2,5 @@ resource "aws_security_group" "rabbitmq" {
   name        = "publishing_platform_rabbitmq_access"
   vpc_id      = data.tfe_outputs.vpc.nonsensitive_values.vpc_id
   description = "Access to the rabbitmq host from its ELB"
+  tags        = { Name = "publishing_platform_rabbitmq_access" }
 }
