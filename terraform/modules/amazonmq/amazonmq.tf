05f969d4439a391ece5a6c53115e09cfdf8c30cd
neilha <neilharrold@gmail.com>
2025-05-12 15:50:23 +0100

    continuers work on  amazonmq

--- terraform/modules/amazonmq/amazonmq.tf
+++ terraform/modules/amazonmq/amazonmq.tf
@@ -6,6 +6,17 @@ locals {
   }[var.amazonmq_deployment_mode]
 }
 
+data "aws_acm_certificate" "internal_cert" {
+  domain   = "*.${var.publishing_platform_environment}.publishing-platform-internal.top"
+  statuses = ["ISSUED"]
+}
+
+data "aws_vpc_endpoint" "mq" {
+  depends_on = [aws_mq_broker.publishing_amazonmq]
+  vpc_id     = data.tfe_outputs.vpc.nonsensitive_values.id
+  tags       = { Broker = aws_mq_broker.publishing_amazonmq.id }
+}
+
 resource "random_password" "mq_user" {
   for_each = toset([
     "root",
@@ -46,5 +57,105 @@ resource "aws_mq_broker" "publishing_amazonmq" {
     console_access = true
     username       = "root"
     password       = random_password.mq_user["root"].result
+  }
+}
+
+resource "aws_lb" "publishingmq_lb_internal" {
+  name               = "publishingamazonmq-lb-internal"
+  tags               = { "Name" = "publishingamazonmq-lb-internal" }
+  internal           = true
+  load_balancer_type = "network"
+  subnets            = aws_mq_broker.publishing_amazonmq.subnet_ids
+
+  access_logs {
+    bucket = "publishing-platform-${var.publishing_platform_environment}-aws-logging"
+    prefix = "lb/publishingamazonmq-internal-lb"
+  }
+}
+
+resource "aws_lb_listener" "internal_https" {
+  load_balancer_arn = aws_lb.publishingmq_lb_internal.arn
+  port              = "443"
+  protocol          = "TLS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = data.aws_acm_certificate.internal_cert.arn
+  tags              = { Description = "MQ admin web UI" }
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.internal_https.arn
+  }
+}
+
+resource "aws_lb_target_group" "internal_https" {
+  name        = "publishingmq-lb-internal-https"
+  target_type = "ip"
+  port        = 443
+  protocol    = "TLS"
+  vpc_id      = data.tfe_outputs.vpc.nonsensitive_values.id
+
+  health_check {
+    path     = "/"
+    protocol = "HTTPS"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "internal_https_ips" {
+  count = local.mq_instance_count
+  depends_on = [
+    aws_mq_broker.publishing_amazonmq,
+    aws_lb_target_group.internal_https,
+  ]
+  target_group_arn = aws_lb_target_group.internal_https.arn
+  target_id        = data.aws_network_interface.mq[count.index].private_ip
+  port             = 443
+}
+
+resource "aws_lb_listener" "internal_amqps" {
+  load_balancer_arn = aws_lb.publishingmq_lb_internal.arn
+  port              = "5671"
+  protocol          = "TLS"
+  certificate_arn   = data.aws_acm_certificate.internal_cert.arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.internal_amqps.arn
+  }
+}
+
+resource "aws_lb_target_group" "internal_amqps" {
+  name        = "publishingmq-lb-internal-amqps"
+  target_type = "ip"
+  port        = 5671
+  protocol    = "TLS"
+  vpc_id      = data.tfe_outputs.vpc.nonsensitive_values.id
+
+  health_check {
+    path     = "/"
+    port     = 443
+    protocol = "HTTPS"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "internal_amqps_ips" {
+  count = local.mq_instance_count
+  depends_on = [
+    aws_mq_broker.publishing_amazonmq,
+    aws_lb_target_group.internal_amqps,
+  ]
+  target_group_arn = aws_lb_target_group.internal_amqps.arn
+  target_id        = data.aws_network_interface.mq[count.index].private_ip
+  port             = 5671
+}
+
+resource "aws_route53_record" "publishing_amazonmq_internal_root_domain_name" {
+  zone_id = data.tfe_outputs.root_dns.nonsensitive_values.internal_root_zone_id
+  name    = "${lower(aws_mq_broker.publishing_amazonmq.broker_name)}.${var.publishing_platform_environment}.publishing-platform-internal.top"
+  type    = "A"
+
+  alias {
+    name                   = aws_lb.publishingmq_lb_internal.dns_name
+    zone_id                = aws_lb.publishingmq_lb_internal.zone_id
+    evaluate_target_health = true
   }
 }
