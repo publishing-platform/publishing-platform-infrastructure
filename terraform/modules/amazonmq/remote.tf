9ca577351d2ed7c41b840511e8bbb30cdaec1bb4
neilha <neilharrold@gmail.com>
2025-05-21 21:48:04 +0100

    deletes amazonmq module

--- terraform/modules/amazonmq/remote.tf
+++
@@ -1,9 +0,0 @@
-data "tfe_outputs" "vpc" {
-  organization = "publishing-platform"
-  workspace    = "vpc-${var.publishing_platform_environment}"
-}
-
-data "tfe_outputs" "shared" {
-  organization = "publishing-platform"
-  workspace    = "shared-${var.publishing_platform_environment}"
-}
