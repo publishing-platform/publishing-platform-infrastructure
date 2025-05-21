95e4f2928b9f7597b88a3ede8af69aff1225ad6a
neilha <neilharrold@gmail.com>
2025-05-21 17:44:26 +0100

    removes unnecessary providers from shared module

--- terraform/modules/shared/terraform.tf
+++ terraform/modules/shared/terraform.tf
@@ -9,11 +9,6 @@ terraform {
   required_version = "~> 1.5"
 
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.5"
-    }
-
     random = {
       source  = "hashicorp/random"
       version = "~> 3.6"
