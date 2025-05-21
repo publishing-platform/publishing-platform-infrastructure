9efce0b8ba55f8c15bb2188327da8d9a39c05aee
neilha <neilharrold@gmail.com>
2025-05-21 17:54:04 +0100

    changes name of output variable

--- terraform/modules/shared/outputs.tf
+++ terraform/modules/shared/outputs.tf
@@ -1,4 +1,4 @@
-output "mq_user_passwords" {
+output "mq_user" {
   description = "A map of Amazon MQ user passwords"
   value       = { for user, pw in random_password.mq_user : user => pw.result }
   sensitive   = true
