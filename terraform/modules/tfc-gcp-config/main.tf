28cb2fbb1053bf038635a8d01a161d3300cc70cb
neilha <neilharrold@gmail.com>
2025-09-26 18:17:37 +0100

    allows deletion of gcp project

--- terraform/modules/tfc-gcp-config/main.tf
+++ terraform/modules/tfc-gcp-config/main.tf
@@ -4,10 +4,12 @@ locals {
 
 resource "google_project" "environment_project" {
   name       = "Search API ${local.display_name}"
-  project_id = "publishing-platform-search-api-${var.publishing_platform_environment}"
+  project_id = "search-api-${var.publishing_platform_environment}"
 
   folder_id       = var.google_cloud_folder
   billing_account = var.google_cloud_billing_account
+
+  deletion_policy = "DELETE" # remove this in production
 
   labels = {
     "programme"                       = var.organization
