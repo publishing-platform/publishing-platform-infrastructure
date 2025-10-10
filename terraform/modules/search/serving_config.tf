10360b2392abed04ac57e754b384a22e458fcb7e
neilha <neilharrold@gmail.com>
2025-10-10 12:34:39 +0100

    debugging

--- terraform/modules/search/serving_config.tf
+++ terraform/modules/search/serving_config.tf
@@ -16,11 +16,11 @@ resource "restapi_object" "discovery_engine_serving_config" {
 
   # Since the default serving config is created automatically with the engine, we need to update
   # even on initial Terraform resource creation
-  create_method = "PATCH"
-  create_path   = "/engines/${google_discovery_engine_search_engine.publishing_platform_engine.engine_id}/servingConfigs/default_search?updateMask=boost_control_ids,synonyms_control_ids"
-  update_method = "PATCH"
-  update_path   = "/engines/${google_discovery_engine_search_engine.publishing_platform_engine.engine_id}/servingConfigs/default_search?updateMask=boost_control_ids,synonyms_control_ids"
-  read_path     = "/engines/${google_discovery_engine_search_engine.publishing_platform_engine.engine_id}/servingConfigs/default_search"
+  create_method  = "PATCH"
+  create_path    = "/engines/${google_discovery_engine_search_engine.publishing_platform_engine.engine_id}/servingConfigs/default_search?updateMask=boost_control_ids,synonyms_control_ids"
+  update_method  = "PATCH"
+  update_path    = "/engines/${google_discovery_engine_search_engine.publishing_platform_engine.engine_id}/servingConfigs/default_search?updateMask=boost_control_ids,synonyms_control_ids"
+  read_path      = "/engines/${google_discovery_engine_search_engine.publishing_platform_engine.engine_id}/servingConfigs/default_search"
   destroy_method = "DELETE"
   destroy_path   = "/engines/${google_discovery_engine_search_engine.publishing_platform_engine.engine_id}/servingConfigs/default_search"
 
@@ -29,4 +29,6 @@ resource "restapi_object" "discovery_engine_serving_config" {
     boostControlIds    = [] # keys(local.boostControls)
     synonymsControlIds = [] # keys(local.synonymControls)
   })
+
+  lifecycle { create_before_destroy = true }
 }
