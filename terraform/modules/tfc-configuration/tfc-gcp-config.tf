6f8acccba67395fa6ef2816fa96aa31d97c25705
neilha <neilharrold@gmail.com>
2025-10-17 12:10:29 +0100

    adds tfc-gcp-config integration workspace

--- terraform/modules/tfc-configuration/tfc-gcp-config.tf
+++ terraform/modules/tfc-configuration/tfc-gcp-config.tf
@@ -1,3 +1,19 @@
+module "tfc-gcp-config-integration" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.10.0"
+
+  organization        = var.organization
+  workspace_name      = "tfc-gcp-config-integration"
+  workspace_desc      = "Creates GCP projects and configures Terraform Cloud as an external OIDC provider."
+  workspace_tags      = ["integration", "tfc", "gcp", "configuration"]
+  terraform_version   = var.terraform_version
+  execution_mode      = "local"
+  global_remote_state = true
+  allow_destroy_plan  = false
+
+  project_name = "tfc-configuration"
+}
+
 module "tfc-gcp-config-production" {
   source  = "alexbasista/workspacer/tfe"
   version = "0.10.0"
