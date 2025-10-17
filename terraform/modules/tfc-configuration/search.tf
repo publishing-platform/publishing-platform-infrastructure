c36ce8be89cb51bd7db5d0cfcd692d12d1ffbec0
neilha <neilharrold@gmail.com>
2025-10-17 11:31:28 +0100

    adds search-secrets workspace

--- terraform/modules/tfc-configuration/search.tf
+++ terraform/modules/tfc-configuration/search.tf
@@ -35,3 +35,37 @@ module "search-production" {
     module.variable-set-search-production
   ]
 }
+
+module "search-secrets-production" {
+  source  = "alexbasista/workspacer/tfe"
+  version = "0.10.0"
+
+  organization        = var.organization
+  workspace_name      = "search-secrets-production"
+  workspace_desc      = "This module manages the AWS secrets required to access GCP Vertex AI Search."
+  workspace_tags      = ["production", "search-secrets", "eks", "aws"]
+  terraform_version   = var.terraform_version
+  execution_mode      = "remote"
+  working_directory   = "/terraform/modules/search-secrets/"
+  trigger_patterns    = ["/terraform/modules/search-secrets/**/*"]
+  global_remote_state = true
+
+  project_name = "publishing-platform-infrastructure"
+
+  vcs_repo = {
+    identifier     = "publishing-platform/publishing-platform-infrastructure"
+    branch         = "main"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+
+  variable_set_names = [
+    "aws-credentials-production",
+    # "common",
+    "common-production"
+  ]
+
+  depends_on = [
+    module.variable-set-aws-credentials-production,
+    module.variable-set-production,
+  ]
+}
