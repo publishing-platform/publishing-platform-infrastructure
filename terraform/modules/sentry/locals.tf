5fd75c1131e1f91e8744f4116dfd5ac232bc6deb
neilha <neilharrold@gmail.com>
2025-10-17 12:42:42 +0100

    adds additional sentry projects

--- terraform/modules/sentry/locals.tf
+++ terraform/modules/sentry/locals.tf
@@ -5,12 +5,14 @@ locals {
     "authenticating-proxy",
     "collections-frontend",
     "content-store",
+    "finder-frontend",
     "frontend",
     "organisations-publisher",
     "publisher",
     "publishing-api",
     "router",
     "router-api",
+    "search-api",
     "signon"
   ]
 }
