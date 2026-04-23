5b9b34561adad02be69e3bd22276b5d712e83a91
neilha <neilharrold@gmail.com>
2026-04-23 21:23:15 +0100

    Update Helm and Kubernetes provider versions to ~> 3.0 and ~> 3.1 respectively

--- terraform/modules/cluster-services/providers.tf
+++ terraform/modules/cluster-services/providers.tf
@@ -21,7 +21,7 @@ provider "kubernetes" {
 provider "helm" {
   # TODO: If/when TF makes provider configs a first-class language object,
   # reuse the identical config from above.
-  kubernetes {
+  kubernetes = {
     host                   = data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_endpoint
     cluster_ca_certificate = base64decode(data.tfe_outputs.cluster_infrastructure.nonsensitive_values.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.cluster_token.token
