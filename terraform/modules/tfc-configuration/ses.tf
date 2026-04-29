5bb13190b0b0baa7f8d52ad18830617318b8731a
neilha <neilharrold@gmail.com>
2026-04-29 08:54:38 +0100

    refactor SES module: streamline variable management and add new tfvars files

--- terraform/modules/tfc-configuration/ses.tf
+++ terraform/modules/tfc-configuration/ses.tf
@@ -1,6 +1,5 @@
 module "ses-production" {
-  source  = "alexbasista/workspacer/tfe"
-  version = "0.15.0"
+  source = "github.com/alphagov/terraform-govuk-tfe-workspacer"
 
   organization        = var.organization
   workspace_name      = "ses-production"
@@ -9,7 +8,12 @@ module "ses-production" {
   terraform_version   = var.terraform_version
   execution_mode      = "remote"
   working_directory   = "/terraform/modules/ses/"
-  trigger_patterns    = ["/terraform/modules/ses/**/*"]
+  trigger_patterns = [
+    "/terraform/modules/ses/**/*",
+    "/terraform/variables/production/common.tfvars",
+    "/terraform/variables/variables-common.tf",
+    "/terraform/variables/production/ses.tfvars"
+  ]
   global_remote_state = true
   allow_destroy_plan  = false
 
@@ -21,16 +25,16 @@ module "ses-production" {
     oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
   }
 
+  tfvars_files = [
+    "production/common.tfvars",
+    "production/ses.tfvars"
+  ]
+
   variable_set_names = [
-    "aws-credentials-production",
-    # "common",
-    "common-production",
-    "ses-production"
+    "aws-credentials-production"
   ]
 
   depends_on = [
-    module.variable-set-aws-credentials-production,
-    module.variable-set-production,
-    module.variable-set-ses-production
+    module.variable-set-aws-credentials-production
   ]
 }
