b4435d35dd210fa2087e9b3ac45afbe6cc6aacf3
neilha <neilharrold@gmail.com>
2026-05-22 11:19:50 +0100

    adds asset-manager efs and s3 resources to shared module

--- terraform/modules/shared/outputs.tf
+++ terraform/modules/shared/outputs.tf
@@ -8,3 +8,13 @@ output "aws_s3_bucket_content_publisher_activestorage_arn" {
   description = "ARN corresponding to the content-publisher active storage s3 bucket."
   value       = aws_s3_bucket.content_publisher_activestorage.arn
 }
+
+output "aws_s3_bucket_assets_arn" {
+  description = "ARN corresponding to the asset manager s3 bucket."
+  value       = aws_s3_bucket.assets.arn
+}
+
+output "assets_efs_id" {
+  description = "ID corresponding to the asset manager EFS."
+  value       = aws_efs_file_system.assets_efs.id
+}
